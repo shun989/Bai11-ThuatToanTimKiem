920697e25348ef1107c73d46360b0f161ac84dd9
shun989 <minhtuanvt89@gmail.com>
2021-04-19 13:53:53 +0700

    up file

--- ThucHanh/timSoLanXuatHienCua1So/index.php
+++ ThucHanh/timSoLanXuatHienCua1So/index.php
@@ -1,1 +1,31 @@
 <?php
+
+function findNumber($array,$value)
+{
+    $count = 0;
+    for ($i = 0;$i < count($array);$i++)
+    {
+        if ($array[$i] == $value)
+        {
+            $count++;
+            $i++;
+        }
+    }
+    return $count;
+}
+
+$numbers = [];
+$value = 68;
+$count = 0;
+for ($i = 0;$i < 150;$i++)
+{
+    $numbers[$i] = rand(1,150);
+}
+foreach ($numbers as $number)
+{
+    echo $number . " ";
+}
+$countValue = findNumber($numbers,$value);
+echo "<br/>";
+echo "Co $countValue so $value ";
+
